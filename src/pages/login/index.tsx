62b2b7ad3421e7bb056cc7ffa4d8fd5691a2cc97
KizoFCB <kizofcb@gmail.com>
2023-08-02 21:44:27 +0300

    feat[RSB1] : Finish the login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,53 @@
-import { Stack, Box, Typography } from "@mui/material";
+import { pathnames } from "routes";
+import theme from "theme";
+import { ErrorMessage } from "@hookform/error-message";
+import {
+  Stack,
+  Box,
+  Typography,
+  FormControl,
+  TextField,
+  Button,
+} from "@mui/material";
 import CoverImage from "assets/cover.jpeg";
+import { useEffect, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const defaultValues = {
+    email: "",
+    password: "",
+  };
+
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+  const {
+    control,
+    formState: { errors, isValid },
+    getValues,
+  } = useForm({
+    mode: "onChange",
+    defaultValues: defaultValues,
+  });
+
+  const handleFormSubmit = async () => {
+    const currentValues = getValues();
+    if (isValid) {
+      try {
+        localStorage.setItem("token", "token");
+        navigate(pathnames.BOOKS);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Stack
       sx={{ width: "100%", height: "100%", background: "lightblue" }}
@@ -22,6 +68,128 @@ const Login = () => {
           <Typography variant="subtitle1">
             Please enter your email address and password to access your account
           </Typography>
+
+          <Stack
+            width={"100%"}
+            gap="20px"
+            component="form"
+            justifyContent="start"
+          >
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: "This field is required",
+                pattern: {
+                  value: /^$/,
+                  message: "Please enter a valid email format",
+                },
+              }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <FormControl>
+                  <TextField
+                    id="email"
+                    value={value}
+                    variant="standard"
+                    type="text"
+                    fullWidth
+                    onChange={(event) => onChange(event.target.value)}
+                    error={error?.message ? true : false}
+                    placeholder="Email address"
+                    InputProps={{ disableUnderline: true }}
+                    sx={{
+                      borderRadius: "8px",
+                      paddingInline: "8px",
+                      minHeight: "50px",
+                      backgroundColor: "#d3d3d3",
+                    }}
+                  />
+                  <ErrorMessage
+                    style={{
+                      color: theme.palette.error.main,
+                      marginTop: "16px",
+                    }}
+                    errors={errors}
+                    name="email"
+                    as="p"
+                  />
+                </FormControl>
+              )}
+            />
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: "This field is required" }}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <FormControl>
+                  <TextField
+                    id="password"
+                    value={value}
+                    fullWidth
+                    type={showPassword ? "text" : "password"}
+                    variant="standard"
+                    onChange={(event) => onChange(event.target.value)}
+                    error={error?.message ? true : false}
+                    placeholder="Password"
+                    InputProps={{
+                      disableUnderline: true,
+                      endAdornment: (
+                        <Box
+                          sx={{ cursor: "pointer" }}
+                          onClick={() => {
+                            setShowPassword((prev) => !prev);
+                          }}
+                        >
+                          {showPassword ? "Hide" : "Show"}
+                        </Box>
+                      ),
+                    }}
+                    sx={{
+                      borderRadius: "8px",
+                      paddingInline: "8px",
+                      minHeight: "50px",
+                      backgroundColor: "#d3d3d3",
+                    }}
+                  />
+                  <ErrorMessage
+                    style={{
+                      color: theme.palette.error.main,
+                      marginTop: "16px",
+                    }}
+                    errors={errors}
+                    name="password"
+                    as="p"
+                  />
+                </FormControl>
+              )}
+            />
+            <Button
+              type="submit"
+              disabled={!isValid}
+              sx={{ marginTop: "4px", maxWidth: "150px", borderRadius: "8px" }}
+              onClick={handleFormSubmit}
+            >
+              Sign in
+            </Button>
+          </Stack>
+
+          <Typography variant="subtitle2">Forgot passowrd?</Typography>
+
+          {/* <Box
+            component="img"
+            src={CoverImage}
+            alt="logo"
+            sx={{
+              maxWidth: "200px",
+              height: "70px",
+            }}
+          /> */}
         </Stack>
         <Box
           component="img"
