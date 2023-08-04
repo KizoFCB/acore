f968535fbe92e966c8b77f341697b53ba8f40f3b
KizoFCB <kizofcb@gmail.com>
2023-08-04 15:37:43 +0300

    feat[RSB7] : Fix the delete modal styles

--- src/components/deleteDialog/index.tsx
+++ src/components/deleteDialog/index.tsx
@@ -1,6 +1,7 @@
 import { IDeleteDialogProps } from "interfaces/components/deleteDialog";
 import {
   Button,
+  useTheme,
   Dialog,
   DialogActions,
   DialogContent,
@@ -13,23 +14,46 @@ const DeleteDialog = ({
   handleClose,
   handleConfirmation,
 }: IDeleteDialogProps) => {
+  const theme = useTheme();
   return (
     <Dialog
       open={open}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      sx={{
+        "& .MuiPaper-root": {
+          borderRadius: "16px",
+          minWidth: "700px",
+          padding: "8px",
+        },
+      }}
     >
       <DialogTitle id="alert-dialog-title">Delete Book?</DialogTitle>
-      <DialogContent>
+      <DialogContent sx={{ marginTop: "8px" }}>
         <DialogContentText id="alert-dialog-description">
-          Are you sure you want to delete this book?
+          Are you sure you want to delete this Book?
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>No</Button>
-        <Button onClick={handleConfirmation} color="primary" autoFocus>
-          Yes
+        <Button
+          onClick={handleClose}
+          sx={{
+            textTransform: "none",
+            borderRadius: "8px",
+            background: theme.palette.grey[500],
+            color: "black",
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleConfirmation}
+          color="error"
+          sx={{ textTransform: "none", borderRadius: "8px" }}
+          variant="contained"
+        >
+          Delete
         </Button>
       </DialogActions>
     </Dialog>
